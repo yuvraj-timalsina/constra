a100c73aa20453150e5724e7937496eeda8fd1af
Yv <raazyuvv@gmail.com>
2022-05-28 09:07:58 +0545

    Typos Fixed!

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,8 +78,8 @@
             <div class="float-right d-none d-sm-block">
                 <b>Version</b> 3.0.5
             </div>
-            <strong>Copyright &copy; {{ date('Y') }} <a href="#">Yv</a>.</strong> All rights
-            reserved.
+            <strong>Copyright &copy; {{ date('Y') }} <a href="#">Yv</a>.</strong> All Rights
+            Reserved.
         </footer>
     </div>
 
