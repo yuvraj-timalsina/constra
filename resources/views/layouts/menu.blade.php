5746ac931c49208a4aaac1be81d5879fe19cc2b7
Yv <raazyuvv@gmail.com>
2022-05-31 21:22:15 +0545

    Update menu.blade.php

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,6 +1,6 @@
 <li class="nav-item">
     <a href="{{ route('dashboard') }}" class="nav-link {{ Request::is('*dashboard*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
+        <i class="nav-icon fas fa-laptop-code"></i>
         <p>Dashboard</p>
     </a>
 </li>
@@ -12,7 +12,7 @@
 </li>
 <li class="nav-item">
     <a href="{{ route('projects.index') }}" class="nav-link {{ Request::is('*projects*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-images"></i>
+        <i class="nav-icon fas fa-briefcase"></i>
         <p>Project</p>
     </a>
 </li>
